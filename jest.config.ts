2a65fb17b481ead9600c0b8d6a5e4c5a89dcea10
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-01-23 16:42:51 +0900

    hotfix / clean up dhiectory

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,24 @@
 import type { Config } from "@jest/types"
 import { defaults } from "jest-config"
-import { pathsToModuleNameMapper } from "ts-jest/utils"
+// import { pathsToModuleNameMapper } from "ts-jest/utils"
 
 // cf. https://www.typescriptlang.org/ja/tsconfig#resolveJsonModule
 // cf. https://www.typescriptlang.org/ja/tsconfig#moduleResolution
-import { compilerOptions } from "./tsconfig.json"
+// import { compilerOptions } from "./tsconfig.json"
 
 /** @typedef {import('ts-jest/dist/types')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
-const config = (): Config.InitialOptions => {
+// eslint-disable-next-line @typescript-eslint/require-await
+const config = async (): Promise<Config.InitialOptions> => {
   // jest configuration goes here: cf. https://jestjs.io/docs/ja/configuration
   return {
     verbose: true,
     notify: true,
     preset: "ts-jest",
     rootDir: ".",
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1"
+    },
     testMatch: [...defaults.testMatch, "**/test/**/*.[jt]s?(x)"],
     testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, "/dist/"],
     testEnvironment: "jsdom",
